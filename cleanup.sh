09a3cba0285b2da11b994b69473642ab9179face
Linh Ngo <lngo@wcupa.edu>
2019-07-19 11:12:36 -0400

    redesigning the approach to this project

---
+++ cleanup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker stop $(docker ps -aq)
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images -q)
--- head/head.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-while :; do sleep 2073600 & wait; done
